d7a265635aad1bc8b19843f8da6fee1a6c0f478b
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-10-05 13:41:36 +0200

    [FEATURE][ROUTER ADAPTER] * Router adapter was implemented

---
+++ src/adapter/express-route-adapter.js
@@ -0,0 +1,17 @@
+module.exports =  adapterRoute = (controller) => {
+    return async (req, res) => {
+        const httpRequest = {
+            body: req.body
+        }
+        const httpResponse = await controller.handle(httpRequest)
+        if (httpResponse.statusCode === 200) {
+            res.status(httpResponse.statusCode).json(httpResponse.body)
+        } else {
+            res.status(httpResponse.statusCode).json({
+                error: httpResponse.message
+            })
+        }
+    }
+}
+
+
